8e98a2d0a8da349806daa318996387fdb029f017
Ahmad Qadri <arqchicago@gmail.com>
2021-05-17 22:58:13 -0400

    created a visual for the original data distribution

--- clt.py
+++ clt.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import scipy.stats as st
 
 # creating a skewed distribution with 1000 observations
 random_data = 1000*np.random.gamma(1.95, 2.5, 1000)
@@ -9,3 +10,9 @@ random_data_df = random_data_df.round(0)
 print(random_data_df)
 
 # visualizing the data distribution
+fig, ax = plt.subplots()
+plt.hist(random_data_df['data'], bins=100)
+plt.title('original data distribution', fontsize=25)
+ax.set_ylabel('Frequency')
+ax.set_xlabel('data')
+plt.savefig('output/orig_distribution.png')
